d6912363ac6aed18e9d045d273f19b9e3d6b83ef
Themis Zamani <themiszamani@gmail.com>
2020-12-08 12:33:08 +0200

    Update seadatacloud-nvs2.sh
    Update to match the changes of the script. 
    NVS new launch
    
    We are very pleased to announce that we have launched NVS with new functionalities and "look and feel".
    So please have a look at http://vocab.nerc.ac.uk
    
    For anybody that was accessing the RDF/XML pages, you will get them back if you change your Accept header to application/rdf+xml or by clicking on the Alternate Profiles--> RDF/XML
    NOTE: Currently the html interface does not show all the mappings although they exist, so we are very sorry about that. 
    This will be corrected on Monday. In the mean time, to see your mappings please click on the Alternate Profiles--> RDF/XML or any other serialization you want. 
    We would love to receive your feedback

--- seadatacloud-nvs2.sh
+++ seadatacloud-nvs2.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-TIMEOUT=10 # default connection timeout 10 seconds
-#URI="http://vocab.nerc.ac.uk/collection"
+#The command ./nerc_xml_check.sh -u http://vocab.nerc.ac.uk/collection/ -d "Oregon State Coastal Management Program"
+#URI="http://vocab.nerc.ac.uk/collection/"
 #DUMMY_STRING="Oregon State Coastal Management Program"
 
-#################################################################
 # function for help message
 usage () {
 cat <<EOF
 Usage: $me [options]
-Script to check endpoint HTTP Status, Validity of XML code and String existance
- 
+Script to check the xml validity of NERC Vocab Collection URL
+and the existence of a predefined Dummy String.
+
 Options:
-  -u, --uri <URI>			Define endpoint URI to check.
-  -d, --dummy <STRING>			Define string to search for.
-  -t, --connect-timeout	<seconds> 	Maximum time allowed for connection (default: 10s)
-  -h, --help				Print this help text.
+  -u, --url <URL>                       URL of Welcome Login Page
+  -d, --dummy <STRING>                  Define string to search for.
+  -t, --connect-timeout <seconds>       Maximum time allowed for connection (default: 10s)
+  -h, --help                            Print this help text.
 EOF
 }
 
-##################################################################
 # function for parsing arguments
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -27,27 +26,21 @@ do
 key="$1"
 
 case $key in
-    -u|--uri)
-    URI="$2"
+    -u|--url)
+    URL="$2"
     shift # past argument
     shift # past value
     ;;
     -d|--dummy)
     DUMMY="$2"
-    if [ -z "$DUMMY" ] || [[ "$DUMMY" =~ ^-.* ]]
-      then
-        echo "UNKNOWN - No DUMMY STRING is defined | UNKNOWN - No DUMMY STRING is defined"
-	shift
-        exit 3
-    fi
     shift # past argument
     shift # past value
     ;;
     -t|--connect-timeout)
     TIMEOUT="$2"
     if [ -z "$TIMEOUT" ] || [[ "$TIMEOUT" =~ ^-.* ]]
-     then
-  	TIMEOUT=10 	# if TIMEOUT is not set, but '-t' option is used, fall to default 10 seconds
+    then
+        TIMEOUT=10      # if TIMEOUT is not set, but '-t' option is used, fall to default 10 seconds
         shift
     else
     shift # past argument
@@ -64,67 +57,57 @@ esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
-################################################################
+
 # Check if required options are passed(empty or dash(-))
-if [ -z "$URI" ] || [[ "$URI" =~ ^-.* ]]
+if [ -z "$URL" ] || [[ "$URL" =~ ^-.* ]]
   then
-	echo "UNKNOWN - No URI is defined | UNKNOWN - No URI is defined"
-       	exit 3
-fi
-
-if [ -z "$DUMMY" ] || [[ "$DUMMY" =~ ^-.* ]]
- then
-	echo "UNKNOWN - No DUMMY STRING is defined | UNKNOWN - No DUMMY STRING is defined"
-	exit 3
-fi
-
-if [ -z "$TIMEOUT" ] || [[ "$TIMEOUT" =~ ^-.* ]]
- then
-        echo "UNKNOWN - No CONNECTION TIMEOUT is defined | UNKNOWN - No CONNECTION TIMEOUT is defined"
+        echo "UNKNOWN - No URL is defined | UNKNOWN - No URL is defined"
         exit 3
 fi
+if [ -z "$DUMMY" ] || [[ "$DUMMY" =~ ^-.* ]]
+ then
+        echo "UNKNOWN - No DUMMY STRING is defined | UNKNOWN - No DUMMY STRING is defined"
+        exit 3
+fi
+if [ -z "$TIMEOUT" ] || [[ "$TIMEOUT" =~ ^-.* ]]
+ then
+	TIMEOUT=10
+fi
+
 
 ################################################################
 # Capture HTTP STATUS CODE in variable
-RESP_TIME=$(curl -IL  -w '%{time_total}\n' -s -o /dev/null ${URI} --connect-timeout ${TIMEOUT})
-STATUS=$(curl -IL  -w '%{http_code}\n' -s -o /dev/null ${URI} --connect-timeout ${TIMEOUT})
+STATUS=$(curl -IL  -w '%{http_code}\n' -s -o /dev/null ${URL})
 
-#CHECK if HTTP STATUS CODE is 200 or 000
+#CHECK if HTTP STATUS CODE is 200
 if [ ${STATUS} -eq 200 ];then
-#	echo "OK - HTTP STATUS CODE is ${STATUS} | http_status_code=${STATUS},"
+#        echo "[+] HTTP STATUS CODE is ${STATUS}"
+	# GET xml with Accept Header
+	curl -s -o vocab.nerc.ac.uk.xml ${URL} -H "Accept: application/rdf+xml" --connect-timeout ${TIMEOUT}
 
 	# Check if the XML CODE is valid and capture it in a variable
-	VALID="$(xmlstarlet val -w ${URI} | grep -w valid)"
+	VALID="$(xmlstarlet val -w vocab.nerc.ac.uk.xml | grep -w valid)"
 	# Capture the RETURN CODE of the above command in a variable
 	VALID_GREPED=$(echo $?)
 	# Check if the RETURN CODE is '0'
 	if [ ${VALID_GREPED} -eq 0 ];then
-#        	echo "OK - The XML code is VALID | xml_valid_code=0, "
-
-		# Check if the DUMMY exists in the returned XML CODE
-		DUMMY_EXIST=$(curl -L ${URI} -s --connect-timeout ${TIMEOUT} | fgrep "$DUMMY")
-		DUMMY_GREPED=$(echo $?)
-
-		if [ ${DUMMY_GREPED} -eq 0 ];then
-		        echo "OK - HTTP STATUS CODE is ${STATUS} - The XML code is VALID - The DUMMY string exists - Response Time = ${RESP_TIME}ms | response_time=${RESP_TIME}[ms], http_status_code=${STATUS}, xml_valid_code=0, dummy_str_exist=0 "
-		        exit 0
-		else
-		        echo "CRITICAL - The DUMMY string does NOT exist | response_time=${RESP_TIME}[ms], http_status_code=${STATUS}, xml_valid_code=0, dummy_string_existance=1"
-		        exit 2
-		fi
-
+#	       echo "[+] The XML code is VALID"
+        
+		# Check if the DUMMY_STRING exists in the returned XML CODE
+        	curl -L ${URL} -s | fgrep "$DUMMY" > /dev/null
+        	if [ $? -eq 0 ];then
+			echo "OK - HTTP STATUS CODE is ${STATUS} - The XML code is VALID - The DUMMY string EXISTS| http_status_code=${STATUS}, xml_valid=0, dummy_str_exists=0"
+                	exit 0
+        	else
+                	echo "CRITICAL - The DUMMY string does NOT EXIST | http_status_code=${STATUS}, xml_valid=0, dummy_str_exists=1"
+                	exit 2
+        	fi
 
 	else
-        	echo "CRITICAL - The XML code is NOT valid | response_time=${RESP_TIME}[ms], http_status_code=${STATUS}, xml_valid_code=1, "
+        	echo "CRITICAL - The XML code is NOT valid| http_status_code=${STATUS}, xml_valid=1,  dummy_str_exists=3"    
         	exit 2
 	fi
-
-elif [ ${STATUS} -eq 000 ]; then
-        echo "UNKNOWN - Connection Timeout | response_time=${RESP_TIME}[ms], http_status_code=${STATUS},"
-        exit 3
 else
-        echo "CRITICAL - HTTP STATUS CODE is ${STATUS} | response_time=${RESP_TIME}[ms], http_status_code=${STATUS}, "
+        echo "CRITICAL - HTTPS STATUS CODE is ${STATUS} | http_status_code=${STATUS}, xml_valid=3, dummy_str_exists=3"
         exit 2
 fi
-
-
